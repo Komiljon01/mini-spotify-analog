977af1a9cb1fef36c437aee5983297a137db5082
Komiljon <nortoshovk@gmail.com>
2024-08-28 11:23:18 +0500

    Added functionality to play music, control range, and display song information

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import Player from "./components/player";
+import Song from "./components/song";
+import "./sass/app.scss";
+import data from "./util";
+
 function App() {
+  const [songs, setSongs] = useState(data);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
-    <div>
-      <h1>Spotify</h1>
+    <div className="app">
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
---
+++ src/components/player.jsx
@@ -0,0 +1,75 @@
+import { useRef, useState } from "react";
+import { FaPause, FaPlay } from "react-icons/fa";
+import { FaBackwardStep, FaForwardStep } from "react-icons/fa6";
+
+function Player({ currentSong, isPlaying, setIsPlaying }) {
+  const audioRef = useRef(null);
+
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  // State
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+
+  const timeUpdateHandler = (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime, duration });
+  };
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input
+          type="range"
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+        />
+        <p>{getTime(songInfo.duration)}</p>
+      </div>
+      <div className="player-control">
+        <FaBackwardStep className="skip-back" />
+
+        {isPlaying ? (
+          <FaPause className="play" onClick={playSongHandler} />
+        ) : (
+          <FaPlay className="play" onClick={playSongHandler} />
+        )}
+
+        <FaForwardStep className="skip-forward" />
+      </div>
+      <audio
+        ref={audioRef}
+        src={currentSong.audio}
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+      ></audio>
+    </div>
+  );
+}
+
+export default Player;
